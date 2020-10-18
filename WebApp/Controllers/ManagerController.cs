2ac24dfade911e485d0f7a9ec4959a61f5a3c08a
com.Kondratov <com.Kondratov@gmail.com>
2020-10-18 11:02:11 +0300

    Delete some views

--- WebApp/Controllers/ManagerController.cs
+++ WebApp/Controllers/ManagerController.cs
@@ -24,23 +24,7 @@ namespace WebApp.Controllers
             return View(await _context.Managers.ToListAsync());
         }
 
-        // GET: Manager/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var manager = await _context.Managers
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (manager == null)
-            {
-                return NotFound();
-            }
-
-            return View(manager);
-        }
+       
 
         // GET: Manager/Create
         public IActionResult AddOrEdit(int id = 0 )
@@ -53,7 +37,7 @@ namespace WebApp.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,SurName")] Manager manager)
+        public async Task<IActionResult> AddOrEdit([Bind("Id,Name,SurName")] Manager manager)
         {
             if (ModelState.IsValid)
             {
@@ -64,56 +48,7 @@ namespace WebApp.Controllers
             return View(manager);
         }
 
-        // GET: Manager/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var manager = await _context.Managers.FindAsync(id);
-            if (manager == null)
-            {
-                return NotFound();
-            }
-            return View(manager);
-        }
-
-        // POST: Manager/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,SurName")] Manager manager)
-        {
-            if (id != manager.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(manager);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ManagerExists(manager.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(manager);
-        }
+      
 
         // GET: Manager/Delete/5
         public async Task<IActionResult> Delete(int? id)
@@ -133,20 +68,6 @@ namespace WebApp.Controllers
             return View(manager);
         }
 
-        // POST: Manager/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var manager = await _context.Managers.FindAsync(id);
-            _context.Managers.Remove(manager);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool ManagerExists(int id)
-        {
-            return _context.Managers.Any(e => e.Id == id);
-        }
+       
     }
 }
